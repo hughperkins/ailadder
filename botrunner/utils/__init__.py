9529f3aa0e55b37bcb62497271e733fe96cbc05f
Hugh Perkins <hughperkins@gmail.com>
2009-10-22 10:12:23 +0800

    Purged botrunner directory, moved webserver directory contents to root

--- botrunner/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-# This file's presence lets us import files from this directory, like:
-# import utils.foo as foo
-
-# This lets us do 'from utils import *':
-__all__ = ['dates', 'stringhelper','tester','filehelper','listhelper','userinput']
-
