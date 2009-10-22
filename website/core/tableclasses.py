df3e86ed68785b9df7fc48b35ca99ce411f0e2b6
Hugh Perkins <hughperkins@gmail.com>
2009-10-22 10:46:49 +0800

    purged botrunner stuff from forms and tableclasses

--- website/core/tableclasses.py
+++ website/core/tableclasses.py
@@ -33,30 +33,6 @@ import loginhelper
 
 Base = sqlalchemy.ext.declarative.declarative_base()
 
-class Map(Base):
-   __tablename__ = 'maps'
-
-   map_id = Column(Integer,primary_key=True)
-   map_name = Column(String(255))
-   map_archivechecksum = Column(String(255))
-   map_url = Column(String(255))
-
-   def __init__(self, map_name, map_archivechecksum ):
-      self.map_name = map_name
-      self.map_archivechecksum = map_archivechecksum
-
-class Mod(Base):
-   __tablename__ = 'mods'
-
-   mod_id =Column(Integer,primary_key=True)
-   mod_name = Column(String(255))
-   mod_archivechecksum = Column(String(255))
-   mod_url = Column(String(255))
-
-   def __init__(self, mod_name, mod_archivechecksum ):
-      self.mod_name = mod_name
-      self.mod_archivechecksum = mod_archivechecksum
-
 class Role(Base):
    __tablename__ = 'roles'
 
@@ -170,93 +146,6 @@ class Cookie(Base):
 
    account = relation("Account")
 
-# I think this was in transition when I switched to doing something else ;-)
-#class BotRunnerOption(Base):
-#   __tablename__ = 'botrunner_options'
-#
-#   botrunner_option_id = Column(Integer,primary_key=True)
-#   botrunner_option_name = Column(String(255))
-
-class BotRunnerSupportedMap(Base):
-   __tablename__ = 'botrunner_supportedmaps'
-
-   botrunner_id = Column(Integer,ForeignKey('botrunners.botrunner_id'),primary_key=True)
-   map_id = Column(Integer,ForeignKey('maps.map_id'),primary_key=True)
-
-   map = relation("Map",uselist = False)
-
-   def __init__(self, map ):
-      self.map = map
-
-class BotRunnerSupportedAI(Base):
-   __tablename__ = 'botrunner_supportedais'
-
-   botrunner_id = Column(Integer,ForeignKey('botrunners.botrunner_id'), primary_key = True)
-   ai_id = Column(Integer,ForeignKey('ais.ai_id'), primary_key = True )
-
-   ai = relation("AI")
-
-   def __init__ (self, ai ):
-      self.ai = ai
-
-class BotRunnerSupportedMod(Base):
-   __tablename__ = 'botrunner_supportedmods'
-
-   botrunner_id = Column(Integer,ForeignKey('botrunners.botrunner_id'),primary_key=True)
-   mod_id = Column(Integer,ForeignKey('mods.mod_id'),primary_key=True)
-
-   mod = relation("Mod")
-
-   def __init__(self, mod ):
-      self.mod = mod
-
-class BotRunnerAssignedOption(Base):
-   __tablename__ = 'botrunner_assignedoptions'
-
-   botrunner_id = Column(Integer,ForeignKey('botrunners.botrunner_id'),primary_key=True)
-   botrunner_option_id = Column(Integer,ForeignKey('aioptions.option_id'),primary_key=True)
-
-   option = relation("AIOption")
-
-   def __init__(self, option ):
-      self.option = option
-
-class BotRunnerSession(Base):
-   __tablename__ = 'botrunner_sessions'
-
-   botrunner_id = Column(Integer,ForeignKey('botrunners.botrunner_id'), primary_key = True )
-   botrunner_session_id = Column(String(255), primary_key = True)
-   lastpingstatus = Column(String(255))
-   lastpingtime = Column(String(255))
-
-   def __init__(self, botrunner_session_id ):
-      self.botrunner_session_id = botrunner_session_id
-
-      self.pingtimeok = False # used by viewbotrunners.py
-      self.lastpingtimestring = '' # used by viewbotrunners.py
-
-class BotRunner(Base):
-   __tablename__ = 'botrunners'
-
-   botrunner_id = Column(Integer,primary_key=True)
-   botrunner_name = Column(String(255))
-   botrunner_sharedsecret = Column(String(255))
-   botrunner_owneraccountid = Column(Integer, ForeignKey('accounts.account_id') )
-   rowspan = 0 # used by viewbotrunners.py
-
-   owneraccount = relation("Account")
-   options = relation("BotRunnerAssignedOption")
-   supportedmaps = relation("BotRunnerSupportedMap", uselist=True )
-   supportedmods = relation("BotRunnerSupportedMod", uselist=True )
-   supportedais = relation("BotRunnerSupportedAI", uselist = True )
-   sessions = relation("BotRunnerSession",uselist = True)
-
-   rowspan = 0
-
-   def __init__( self, botrunner_name, botrunner_sharedsecret ):
-      self.botrunner_name = botrunner_name
-      self.botrunner_sharedsecret = botrunner_sharedsecret
-
 class AIOption(Base):
    __tablename__ = 'aioptions'
 
@@ -265,69 +154,6 @@ class AIOption(Base):
 
    def __init__(self, option_name):
       self.option_name = option_name
-
-class MatchRequestOption(Base):
-   __tablename__ = 'matchrequest_options'
-
-   matchrequest_id = Column(Integer,ForeignKey('matchrequestqueue.matchrequest_id'), primary_key=True)
-   option_id = Column(Integer,ForeignKey('aioptions.option_id'),primary_key=True)
-
-   option = relation("AIOption")
-
-   def __init__(self, option ):
-      self.option = option
-
-class MatchRequest(Base):
-   __tablename__ = 'matchrequestqueue'
-
-   matchrequest_id=Column(Integer,primary_key=True)
-   map_id =Column(Integer, ForeignKey('maps.map_id'))
-   mod_id =Column(Integer, ForeignKey('mods.mod_id'))
-   ai0_id = Column(Integer, ForeignKey('ais.ai_id'))
-   ai1_id = Column(Integer, ForeignKey('ais.ai_id'))
-   league_id = Column( Integer, ForeignKey('leagues.league_id'))
-
-   map = relation("Map" )
-   mod = relation("Mod" )
-   ai0 = relation("AI", primaryjoin = ai0_id == AI.ai_id )
-   ai1 = relation("AI", primaryjoin = ai1_id == AI.ai_id )
-
-   matchrequestinprogress = relation("MatchRequestInProgress", uselist=False)
-   matchresult = relation("MatchResult", uselist=False)
-   options = relation("MatchRequestOption")
-   league = relation("League", uselist=False)
-
-   def __init__( self, ai0, ai1, map, mod, league = None ):
-      self.ai0 = ai0
-      self.ai1 = ai1
-      self.map = map
-      self.mod = mod
-      self.league = league
-
-class MatchRequestInProgress(Base):
-   __tablename__ = 'matchrequests_inprogress'
-
-   matchrequest_id = Column(Integer,ForeignKey('matchrequestqueue.matchrequest_id'),primary_key=True )
-   botrunner_id = Column(Integer,ForeignKey('botrunners.botrunner_id'),ForeignKey('botrunner_sessions.botrunner_id'))
-   botrunner_session_id = Column(String(255),ForeignKey('botrunner_sessions.botrunner_session_id'))
-   datetimeassigned = Column(String(255))
-
-   botrunner= relation("BotRunner")
-   botrunnersession = relation("BotRunnerSession", primaryjoin=and_( botrunner_id == BotRunnerSession.botrunner_id, botrunner_session_id == BotRunnerSession.botrunner_session_id ) )
-
-   def __init__(self, botrunner, botrunnersession, datetimeassigned ):
-      self.botrunner = botrunner
-      self.botrunnersession = botrunnersession
-      self.datetimeassigned = datetimeassigned
-
-class MatchResult(Base):
-   __tablename__ = 'matchresults'
-
-   matchrequest_id = Column(Integer,ForeignKey('matchrequestqueue.matchrequest_id'),primary_key=True )
-   matchresult = Column(String(255))
-
-   def __init__(self, matchresult ):
-      self.matchresult = matchresult
 
 class League(Base):
    __tablename__ = 'leagues'
@@ -449,14 +275,12 @@ def addstaticdata(session):
    modadminrole = Role("modadmin")
    mapadminrole = Role("mapadmin")
    leagueadminrole = Role("leagueadmin")
-   botrunneradminrole = Role("botrunneradmin")
 
    session.add(accountadminrole)
    session.add(aiadminrole)
    session.add(modadminrole)
    session.add(mapadminrole)
    session.add(leagueadminrole)
-   session.add(botrunneradminrole)
 
    account = Account("admin","admin", "admin")
    session.add(account)
@@ -465,7 +289,6 @@ def addstaticdata(session):
    account.addRole( modadminrole )
    account.addRole( mapadminrole )
    account.addRole( leagueadminrole )
-   account.addRole( botrunneradminrole )
 
    session.add(Account("guest","guest","guest"))
 
