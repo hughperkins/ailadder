f6cdd89479bfd2927966bc68518e66cc4da39f53
Hugh Perkins <hughperkins@gmail.com>
2009-10-26 11:13:53 +0800

    added spring grid url at bottom of each page

--- website/core/jinjahelper.py
+++ website/core/jinjahelper.py
@@ -21,11 +21,13 @@
 
 import jinja2
 import menu
+import confighelper
 
 # renders templatename, passing in named args
 def rendertemplate( templatename, **args ):
    if not args.has_key('menus'):
       args['menus'] = menu.getmenus()
+   args['springgridwebsite'] = confighelper.getValue('springgridwebsite')
    env = jinja2.Environment( loader=jinja2.PackageLoader('jinjaapplication', 'templates'))
    template = env.get_template(templatename)
    print template.render( **args )
